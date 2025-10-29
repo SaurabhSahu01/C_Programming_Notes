25c2c688fafd26a13dc4b65d7336d9b39f06ba32
SaurabhSahu01 <saurabhsahu794@gmail.com>
2025-10-29 10:09:05 +0530

    lab - 2 - 6

--- IT101-P Lab/Exercise-2/6.c
+++ IT101-P Lab/Exercise-2/6.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 
-int main()
-{
-    int n, i, sum = 0;
-
+// Method 1
+int main(){
+    int n;
     scanf("%d", &n);
 
-    for (i = 1; i <= n; i++)
-    {
-        sum += (i * (i + 1)) / 2;
+    int sum = 0;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            sum += j;
+        }
     }
+    printf("%d\n", sum);
+    return 0;
+}
 
+// Method 2
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    int sum = 0;
+    for(int i = 1; i <= n; i++){
+        sum += (i*(i+1))/2;
+    }
+    printf("%d\n", sum);
+    return 0;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int sum = (n*(n+1)*(n+2))/6;
     printf("%d\n", sum);
     return 0;
 }
